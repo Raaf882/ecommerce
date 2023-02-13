d0df47f4adc36869a47ebb56e022025d7c49533f
Raaf882 <rahaf88200@gmail.com>
2023-02-13 18:28:04 +0300

    new setup

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -25,21 +25,21 @@ class ProductSeeder extends Seeder
             'description'=>'a smart tv  with  32 inch and much more features',
             'gallery'=>'https://umg.sa/pub/media/catalog/product/cache/b8954327bf0e7adf2e2e5b0b3c7d4ad0/k/d/kdg32rr672ant-1.jpg',        
         ],
-        // [
-        //     'name'=>'apple iphone',
-        //     'price'=>'300',
-        //     'category'=>'mobile',
-        //     'description'=>'a smarthpone with 5gb ram and much more features',
-        //     'gallery'=>'https://cdn.dxomark.com/wp-content/uploads/medias/post-132367/Apple-iPhone-14_Plus_FINAL_featured-image-packshot-review.jpg',
+        [
+            'name'=>'apple iphone',
+            'price'=>'300',
+            'category'=>'mobile',
+            'description'=>'a smarthpone with 5gb ram and much more features',
+            'gallery'=>'https://cdn.dxomark.com/wp-content/uploads/medias/post-132367/Apple-iPhone-14_Plus_FINAL_featured-image-packshot-review.jpg',
         
-        // ],
-        // [
-        //     'name'=>'LG mobile',
-        //     'price'=>'200',
-        //     'category'=>'mobile',
-        //     'description'=>'a smarthpone with 4gb ram and much more features',
-        //     'gallery'=>'https://cdn.dxomark.com/wp-content/uploads/medias/post-125428/Apple-iPhone-14-Pro-Max_FINAL_featured-image-packshot-review-1.jpg',        
-        // ],   
+        ],
+        [
+            'name'=>'LG mobile',
+            'price'=>'200',
+            'category'=>'mobile',
+            'description'=>'a smarthpone with 4gb ram and much more features',
+            'gallery'=>'https://cdn.dxomark.com/wp-content/uploads/medias/post-125428/Apple-iPhone-14-Pro-Max_FINAL_featured-image-packshot-review-1.jpg',        
+        ],   
         
         ]);
     }
